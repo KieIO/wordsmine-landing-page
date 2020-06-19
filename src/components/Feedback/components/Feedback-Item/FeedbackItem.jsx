2cbbb1ffaeabe9c10d4ace14ed82ee85606f3849
Dũng Võ <vodung0801@gmail.com>
2020-06-19 10:35:56 +0700

    Change FeedbackItem Component

--- src/components/Feedback/components/Feedback-Item/FeedbackItem.jsx
+++ src/components/Feedback/components/Feedback-Item/FeedbackItem.jsx
@@ -13,6 +13,17 @@ const FeedbackItem = () => {
         //         />
         //     </Card>
         // </Col>
+
+        // <div className="feedback__item">
+        //     <div className="feedback__info">
+        //         <h3 className="feedback__name">Goware</h3>
+        //         <p className="feedback__location">Vietnam</p>
+        //     </div>
+        //     <div className="feedback__description">
+        //         <p>Words, mine! has helped me to understand just how fun and important and fun studying can be! This school year, in chemistry class I put my terms on Words, mine! and I already feel better about my upcoming test.</p>
+        //     </div>
+        // </div>
+
         <div className="feedback__carousel__item">
             <div className="feedback__carousel__item__info">
                 <h3 className="feedback__carousel__item__info__name">Goware</h3>
