cef316436bdba964edae5f1283125da56a2efe1b
Tran Ly <tranly237@gmail.com>
2020-06-20 23:39:47 +0700

    edit header animation

--- src/components/Header/components/HeaderBurgerMenu/HeaderBurgerMenu.component.jsx
+++ src/components/Header/components/HeaderBurgerMenu/HeaderBurgerMenu.component.jsx
@@ -1,39 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { CATEGORY_HEADER } from '../../../../utils/constant';
 import HeaderCategoryItem from '../HeaderCategoryItem/HeaderCategoryItem';
 import Logo from '../Logo/Logo';
-import { useState } from 'react';
 
 const HeaderBurgerMenu = () => {
     const [isOpen, setIsOpen] = useState(false)
     const toggleIsOpen = (e) => {
         e.preventDefault()
+
+        const eleIcon = document.getElementById("header-burger-menu__icon")
+        if (eleIcon) { 
+            eleIcon.classList.toggle("header-burger-menu__icon-change");
+        }
         
         const element = document.getElementById("header-menu")
         if (element) {
-            if (isOpen) {
-                element.classList.toggle("header-burger-meu__content--open")
-            } else {
-                element.classList.toggle("header-burger-meu__content--open")
-            }
+            element.classList.toggle("header-burger-menu__content--open")
         }
         setIsOpen(!isOpen)
     }
 
     return (
-        <div className="header-burger-meu">
-            <div className="header-burger-meu__top">
+        <div className="header-burger-menu">
+            <div className="header-burger-menu__top">
                 <Logo />
-                <div className="header-burger-meu__icon"  onClick={toggleIsOpen}>
+                {/* <div className="header-burger-menu__icon"  onClick={toggleIsOpen}>
                     <span className="lnr lnr-menu"></span>
+                </div> */}
+                <div className="header-burger-menu__icon" id="header-burger-menu__icon" onClick={toggleIsOpen}>
+                    {/* <span class="lnr lnr-menu"></span> */}
+                    <div class="icon-buger-container">
+                        <div class="bar1"></div>
+                        <div class="bar2"></div>
+                        <div class="bar3"></div>
+                    </div>
                 </div>
             </div>
 
-                <div id="header-menu" className="header-burger-meu__content">
-                    <ul className="header-burger-meu__list">
+                <div id="header-menu" className="header-burger-menu__content">
+                    <ul className="header-burger-menu__list">
                         {
                             CATEGORY_HEADER.map((item, index) => (
-                                <li className="header-burger-meu__item" key={index}>
+                                <li className="header-burger-menu__item" key={index}>
                                     <HeaderCategoryItem {...item} />
                                 </li>
                             ))
