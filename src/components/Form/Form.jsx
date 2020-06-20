6f180a46b0101e53bebcb94ef1ba407b7a7e01e3
Tran Ly <tranly237@gmail.com>
2020-06-20 16:26:10 +0700

    form UI

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,5 @@
 
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Form, Input, Button, Checkbox } from 'antd';
 import ButtonCustom from '../ButtonCustom/ButtonCustom';
 import CustomInput from '../CustomInput/CustomInput';
@@ -12,14 +12,21 @@ const layout = {
 
 const FormSignUp = () => {
     const [form] = Form.useForm();
-    const [checkNick, setCheckNick] = useState(false);
+    const [checkAgreePolicy, setcheckAgreePolicy] = useState(false);
 
-    useEffect(() => {
-        form.validateFields(['nickname']);
-    }, [checkNick]);
+    // const validateMessages = {
+    //     required: '${label} is required!',
+    //     types: {
+    //         email: '${label} is not validate email!',
+    //     },
+    // };
+
+    // useEffect(() => {
+    //     form.validateFields(['isCheckBox']);
+    // }, [checkAgreePolicy, form]);
 
     const onCheckboxChange = e => {
-        setCheckNick(e.target.checked);
+        setcheckAgreePolicy(e.target.checked);
     };
 
     const submit = async (values) => {
@@ -60,146 +67,77 @@ const FormSignUp = () => {
 
             const result = await submit(values)
             console.log("result: ", result)
-    } catch (err) {
-        console.log("err: ", err)
+        } catch (err) {
+            console.log("err: ", err)
+        }
     }
-}
 
-        const onChangeBox = (e) => {
+    const onChangeBox = (e) => {
         console.log(`checked = ${e.target.checked}`);
     }
 
     return (
         <section className="form">
-        <Form form={form} name="dynamic_rule">
-            <Form.Item
-                name="fullName"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your full name',
-                    },
-                ]}
-            >
+            <Form form={form} name="dynamic_rule">
+                <Form.Item
+                    name="fullName"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your full name',
+                        },
+                    ]}
+                >
                     <div className="custom-input">
                         <span className="custom-input__placeholder">Your fullname</span>
                         <Input placeholder="Fullname" />
                     </div>
-            </Form.Item>
-            <Form.Item
-                name="email"
-                type="email"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your email',
-                    },
-                ]}
-            >
+                </Form.Item>
+                <Form.Item
+                    name="email"
+                    type="email"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your email',
+                        },
+                        {
+                            type: 'email',
+                            message: 'Email is not valid',
+
+                        }
+                    ]}
+                >
                     <div className="custom-input">
                         <span className="custom-input__placeholder">Email</span>
-                        <Input placeholder="example@email.com" />
+                        <Input placeholder="example@email.com" type="email" />
                     </div>
-            </Form.Item>
+                </Form.Item>
 
-            <Form.Item
-                name="interest"
-                // label="Your interest"
-            >
-                    {/* <CustomInput label="Your interest" placeholder="Ex: Sport, Coffee, Biology,.."/> */}
+                <Form.Item
+                    name="interest"
+                >
                     <div className="custom-input">
                         <span className="custom-input__placeholder">Your interest</span>
                         <Input placeholder="Ex: Sport, Coffee, Biology,.." />
                     </div>
-            </Form.Item>
-            
-            <Form.Item >
-                <Checkbox checked={checkNick} onChange={onCheckboxChange}>
-                    Nickname is required
-                </Checkbox>
-            </Form.Item>
+                </Form.Item>
 
-            <Form.Item >
-                <Checkbox checked={checkNick}  onChange={onChangeBox}> I agree to the terms of service and privacy policy.</Checkbox>
-            </Form.Item>
-            <Form.Item >
-                <Button type="primary" onClick={onCheck}>
-                    Check
-        </Button>
-            </Form.Item>
-        </Form>
+
+                {/* <Form.Item 
+                name="isCheckBox"
+                >
+                    <Checkbox checked={checkAgreePolicy} onChange={onCheckboxChange}> I agree to the terms of service and privacy policy.</Checkbox>
+                </Form.Item> */}
+
+                <Form.Item >
+                    <ButtonCustom type="primary" onClick={onCheck}>
+                        Check
+                </ButtonCustom>
+                </Form.Item>
+            </Form>
         </section>
     );
-
-    // const [form] = Form.useForm();
-    // const onFinish = () => {
-    //     console.log('on finish')
-    // }
-
-
-    // const onChange = (e) => {
-    //     console.log(`checked = ${e.target.checked}`);
-    // }
-
-    // const validateMessages =  {
-    //     required: '${label} is required!',
-    //     types: {
-    //         email: '${label} is not validate email!',
-    //         number: '${label} is not a validate number!',
-    //     },
-    //     number: {
-    //         range: '${label} must be between ${min} and ${max}',
-    //     },
-    // };
-
-    // const onCheck = async () => {
-    //     try {
-    //         const values = await form.validateFields();
-    //         console.log('Success:', values);
-    //     } catch (errorInfo) {
-    //         console.log('Failed:', errorInfo);
-    //     }
-    // };
-
-
-    // return (
-    //     <section className="form">
-    //         <Form {...layout} validateMessages={validateMessages}
-    //             name="basic"
-    //             initialValues={{ remember: true }}
-    //             onFinish={onFinish} >
-    //             <Form.Item
-    //                 label="Fullname"
-    //                 name="username"
-    //                 rules={[{ required: true, message: 'Please input your fullname!' }]}>
-    //                 <Input placeholder="Please input your fullname" />
-    //             </Form.Item>
-    //             <Form.Item name={['user', 'email']} label="Email" rules={[{ required: true, type: 'email' }]}>
-    //                 <Input placeholder="Please input your email" />
-    //             </Form.Item>
-
-    //             <Form.Item name={['user', 'introduction']} label="Your Interests">
-    //                 <Input.TextArea placeholder="Your Interest.  Ex: Sport, Coffee, Biology,.." />
-    //             </Form.Item>
-
-    //             <div class="form___checkbox-disflex">
-    //                 <Checkbox onChange={onChange}> </Checkbox>
-    //                 <span>I agree to the terms of service and privacy policy.</span>
-    //             </div>
-    //             <Form.Item >
-    //             {/* <div class="form__btn-format" htmlType="submit"> */}
-    //                 <ButtonCustom >Submit</ButtonCustom>
-    //             {/* </div> */}
-    //                 <Button type="primary" onClick={onCheck}>
-    //                     Check
-    //     </Button>
-
-
-    //             </Form.Item>
-    //         </Form>
-
-    //     </section>
-    // );
 }
 
 
