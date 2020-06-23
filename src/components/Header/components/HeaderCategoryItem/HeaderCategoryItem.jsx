f3b2b3b885528b8c7e5e8c2bda7b62953feed629
Tran Ly <tranly237@gmail.com>
2020-06-23 10:01:11 +0700

    edit category item display

--- src/components/Header/components/HeaderCategoryItem/HeaderCategoryItem.jsx
+++ src/components/Header/components/HeaderCategoryItem/HeaderCategoryItem.jsx
@@ -4,7 +4,10 @@ import React from 'react';
 const HeaderCategoryItem = ({target, name}) => {
     return (
         <a className="header-category-item" href={`#${target}`}>
+            {/* <div> */}
             {name}
+
+            {/* </div> */}
         </a>
     );
 }
