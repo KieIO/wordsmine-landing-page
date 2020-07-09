c10a1613eecd4e8117d9f652a4316c8245bbb2a6
Tran Ly <tranly237@gmail.com>
2020-07-09 13:52:13 +0700

    connnect api success

--- src/components/api/user.api.js
+++ src/components/api/user.api.js
@@ -1,39 +1,25 @@
-import { API_URL } from "./apiUrl";
+import { serializeForm, API_URL } from './utils.api';
+
+const axios = require('axios');
 
 
 export const HTTP_STATUS_SUCCESS = 200;
 
-   export const login  = async (data) => {
-        let status = 400;
-        
-       var formData = new FormData();
-
-       for (var k in data) {
-           formData.append(k, data[k]);
-       }
-
-        return fetch(`${API_URL}/login`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            // 'Content-type': 'application/json; charset=UTF-8',
-            // body: JSON.stringify({...data}),
-            body: formData
+export const login = async (data) => {
+    return axios({
+        method: 'post',
+        url: `${API_URL}/login`,
+        data: serializeForm(data),
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    })
+        .then(function (response) {
+            //handle success
+            console.log(response);
+            return response
         })
-            .then((response) => {
-                status = response.status;
-                return response.json();
-            })
-            .then((result) => {
-                if (status === HTTP_STATUS_SUCCESS) {
-                    return result;
-                }
-                if (result && result.message) {
-                    throw new Error(result.message);
-                } else {
-                    throw new Error('Có lỗi xảy ra. Vui lòng thử lại');
-                }
-            })
-            .catch((err) => {
-                throw err;
-            });
-    };
+        .catch(function (response) {
+            //handle error
+            console.log(response);
+            throw response
+        });
+};
