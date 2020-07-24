c1325ec1314de457765fdab30563a8e8ae309ed1
Phan An <phananproduct@gmail.com>
2020-07-24 19:08:28 +0700

    Change copoy

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -1,37 +1,93 @@
-import React from 'react'
-import ImgValue1 from '../../img/value1.png'
-import ImgValue2 from '../../img/value2.png'
+import React from "react";
+import ImgValue1 from "../../img/value1.png";
+import ImgValue2 from "../../img/value2.png";
 
-import './Feature.scss';
+import "./Feature.scss";
 
 const Feature = () => {
-    return (
-        <section className="feature u-padding-horizontal-section" id="feature">
-            <div className="feature__disflex">
-                <div className="feature__img col-6">
-                    <img src={ImgValue1} alt="feature" ></img>
-                </div>
-                <div className="col-6 feature__content-padding-left">
-                    <h2>Your next win is just a fact away</h2>
-                    <p>From flashcards to help you learn francais, to games that make it easy to get a handle on history, use a variety of tools to conquer any challenge.</p>
-                </div>
-            </div>
+  return (
+    <section className="feature u-padding-horizontal-section" id="feature">
+      <div className="feature__disflex">
+        <div className="feature__img col-6">
+          <img src={ImgValue1} alt="feature"></img>
+        </div>
+        <div className="col-6 feature__content-padding-left">
+          <h2>Save words from selection</h2>
+          <p>You can collect any words from any pages.</p>
+        </div>
+      </div>
 
-            <div className="feature__disflex">
-                <div className="col-6 feature__content-padding">
-                    <h2>Don’t get frustrated. Get it done.</h2>
-                    <p>From flashcards to help you learn francais, to games that make it easy to get a handle on history, use a variety of tools to conquer any challenge.</p>
-                </div>
+      <div className="feature__disflex">
+        <div className="col-6 feature__content-padding">
+          <h2>Save words from clipboard</h2>
+          <p>
+            You are in hurry? Don't worry, we also allow to you to save words on
+            the go while surfing the net.
+          </p>
+        </div>
 
-                <div className="feature__img col-6">
-                    <img src={ImgValue2} alt="feature" ></img>
-                </div>
-            </div>
+        <div className="feature__img col-6">
+          <img src={ImgValue2} alt="feature"></img>
+        </div>
+      </div>
 
+      <div className="feature__disflex">
+        <div className="col-6 feature__content-padding">
+          <h2>Collect words from mobile web</h2>
+          <p>
+            You are in hurry? Don't worry, we also allow to you to save words on
+            the go while surfing the net.
+          </p>
+        </div>
 
+        <div className="feature__img col-6">
+          <img src={ImgValue2} alt="feature"></img>
+        </div>
+      </div>
 
-        </section>
-    );
-}
+      <div className="feature__disflex">
+        <div className="col-6 feature__content-padding">
+          <h2>Use flashcard mode to memorize your learning</h2>
+          <p>
+            You are in hurry? Don't worry, we also allow to you to save words on
+            the go while surfing the net.
+          </p>
+        </div>
+
+        <div className="feature__img col-6">
+          <img src={ImgValue2} alt="feature"></img>
+        </div>
+      </div>
+
+      <div className="feature__disflex">
+        <div className="col-6 feature__content-padding">
+          <h2>Lookup for definition from reliable sources</h2>
+          <p>
+            You are in hurry? Don't worry, we also allow to you to save words on
+            the go while surfing the net.
+          </p>
+        </div>
+
+        <div className="feature__img col-6">
+          <img src={ImgValue2} alt="feature"></img>
+        </div>
+      </div>
+
+      <div className="feature__disflex">
+        <div className="col-6 feature__content-padding">
+          <h2>Customize your definition as you want</h2>
+          <p>
+            You are in hurry? Don't worry, we also allow to you to save words on
+            the go while surfing the net.
+          </p>
+        </div>
+
+        <div className="feature__img col-6">
+          <img src={ImgValue2} alt="feature"></img>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Feature;
