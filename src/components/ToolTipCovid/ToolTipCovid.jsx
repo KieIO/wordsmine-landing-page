79a9c099916dc75f859fa3adde32536069ccff81
Dũng Võ <vodung0801@gmail.com>
2020-07-28 14:59:21 +0700

    Add transition smooth when close tooltip covid-19

--- src/components/ToolTipCovid/ToolTipCovid.jsx
+++ src/components/ToolTipCovid/ToolTipCovid.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import './ToolTipCovid.scss';
 
 const ToolTipCovid = (props) => {
+    const { t } = useTranslation();
+
     return (
-        <div className="tooltip-covid19" style={props.isCloseToolTip ? {display: 'none'} : {display: 'flex'}}>
-            <p className="content-tooltip">Stay Home And Take Your Learning With You!</p>
+        <div className="tooltip-covid19" style={props.isCloseToolTip ? { opacity: 0, transition: 'opacity .5s ease-out' } : { opacity: 1 }}>
+            <p className="content-tooltip">{t('toolTipContent')}</p>
             <p className="close-tooltip" onClick={props.closeToolTip}>X</p>
         </div>
     );
