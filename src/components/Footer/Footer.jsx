64ce030a1569b9f0d3f3843342ac279af840447f
Dũng Võ <vodung0801@gmail.com>
2020-07-29 14:16:41 +0700

    Duplicate Another Landing Page with Vietnamese Language

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ import SocialFooter from "./components/SocialFooter/SocialFooter";
 import Copyright from "./components/Copyright/Copyright";
 
 import "./Footer.scss";
-// import ChangeLanguage from "../ChangeLanguage/ChangeLanguage";
+import ChangeLanguage from "../ChangeLanguage/ChangeLanguage";
 
 const Footer = () => {
   const [footerColumns] = useState([
@@ -67,7 +67,7 @@ const Footer = () => {
         </Row>
       </div>
       <div className="footer__bottom">
-        {/* <ChangeLanguage /> */}
+        <ChangeLanguage />
         <SocialFooter />
         <Copyright />
       </div>
